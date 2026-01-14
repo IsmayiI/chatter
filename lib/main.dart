e2bb4518d17f718497b9c8630b9196f2ebc14496
Ismayil <ismayilov_ismayil_97@mail.ru>
2026-01-14 14:57:55 +0400

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:chatter/colors.dart';
+import 'package:chatter/firebase_options.dart';
 import 'package:chatter/responsive/responsive_layout.dart';
 import 'package:chatter/screens/mobile_screen_layout.dart';
 import 'package:chatter/screens/web_screen_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
